ef520bf90977542c4272f654661000048acc2adc
pengh618 <pengh618@gmail.com>
2025-04-13 21:17:01 +0800

    4-13-a

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -4,26 +4,26 @@
             <div class="m-12 grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-4">
                 <n-card class="title-center cursor-pointer" @click="pushRoute('/dream')">
                     <div class="flex items-center">
-                        <img src="https://free4.yunpng.top/2025/04/06/67f27fe10ed35.png" class="w-1/2">
+                        <img src="https://free4.yunpng.top/2025/04/13/67fbb86a5f633.png" class="w-1/2">
                         <span class="ml-4 text-lg">AI解梦</span>
                     </div>
                 </n-card>
                 <n-card class="title-center cursor-pointer" @click="pushRoute('/song')">
                     <div class="flex items-center">
-                        <img src="https://free4.yunpng.top/2025/04/06/67f27fdba0a11.png" class="w-1/2">
+                        <img src="https://free4.yunpng.top/2025/04/13/67fbb871a358e.png" class="w-1/2">
                         <span class="ml-4 text-lg">歌词解读</span>
                     </div>
                 </n-card>
                 <n-card class="title-center cursor-pointer" @click="pushRoute('/plan')">
                     <div class="flex items-center">
-                        <img src="https://free4.yunpng.top/2025/04/06/67f27fe02e8f7.png" class="w-1/2">
+                        <img src="https://free4.yunpng.top/2025/04/13/67fbb86f99364.png" class="w-1/2">
                         <span class="ml-4 text-lg">旅程规划</span>
                     </div>
                 </n-card>
                 <n-card class="title-center">
                     <div class="flex items-center">
-                        <img src="https://free4.yunpng.top/2025/04/06/67f27fdb9dc1f.png" class="w-1/2">
-                        <span class="ml-4 text-lg">AI大厨</span>
+                        <img src="https://free4.yunpng.top/2025/04/13/67fbb86a8969a.png" class="w-1/2">
+                        <span class="ml-4 text-lg">好书概读</span>
                     </div>
                 </n-card>
             </div>
